2d4f9db4a619aec12681ca6b0e05c0ef227e42f7
Alejandro Sanchez <alejandro862003@gmail.com>
2025-08-11 20:27:39 -0300

    firt commit

---
+++ admin/src/assets/all_product.js
@@ -0,0 +1,329 @@
+import p1_img from "./producto_1.jpeg";
+import p2_img from "./producto_2.jpeg";
+import p3_img from "./producto_3.jpeg";
+import p4_img from "./producto_4.jpeg";
+import p5_img from "./producto_5.jpeg";
+import p6_img from "./producto_6.jpeg";
+import p7_img from "./producto_7.jpeg";
+import p8_img from "./producto_8.jpeg";
+import p9_img from "./producto_9.jpeg";
+import p10_img from "./producto_10.jpeg";
+import p11_img from "./producto_11.jpeg";
+import p12_img from "./producto_12.jpeg";
+import p13_img from "./producto_13.jpeg";
+import p14_img from "./producto_14.jpeg";
+import p15_img from "./producto_15.jpeg";
+import p16_img from "./producto_16.jpeg";
+import p17_img from "./producto_17.jpeg";
+import p18_img from "./producto_18.jpeg";
+import p19_img from "./producto_19.jpeg";
+import p20_img from "./producto_20.jpeg";
+import p21_img from "./producto_21.jpeg";
+import p22_img from "./producto_22.jpeg";
+import p23_img from "./producto_23.jpeg";
+import p24_img from "./producto_24.jpeg";
+import p25_img from "./producto_25.jpeg";
+import p26_img from "./producto_26.jpeg";
+import p27_img from "./producto_27.jpeg";
+import p28_img from "./producto_28.jpeg";
+import p29_img from "./producto_29.jpeg";
+import p30_img from "./producto_30.jpeg";
+import p31_img from "./producto_31.jpeg";
+import p32_img from "./producto_32.jpeg";
+import p33_img from "./producto_33.jpeg";
+import p34_img from "./producto_34.jpeg";
+import p35_img from "./producto_35.jpeg";
+import p36_img from "./producto_36.jpeg";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 5,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p5_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 6,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 7,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p7_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 8,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p8_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 9,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p9_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 10,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p10_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 11,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p11_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 12,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "puertas",
+    image: p12_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 13,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p13_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p14_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 15,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p15_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 16,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p16_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p17_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 18,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p18_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 19,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p19_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 20,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p20_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 21,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p21_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 22,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p22_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 23,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p23_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 24,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "parrillas",
+    image: p24_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 25,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p25_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 26,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p26_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 27,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p27_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 28,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p28_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 29,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p29_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 30,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p30_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 31,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p31_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 32,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p32_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 33,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p33_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 34,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p34_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 35,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p35_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 36,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "ventanas",
+    image: p36_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+];
+
+export default all_product;
